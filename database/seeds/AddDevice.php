c877b6e0f6d4e384cb3b668c957af49229b084f0
UnAfraid <unafraid89@gmail.com>
2019-05-22 21:45:43 +0300

    Adding database seed for devices

---
+++ database/seeds/AddDevice.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Device;
+
+class AddDevice extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $devices = [
+            ['name' => 'Test device 1', 'address' => 'Test address 1', 'manufacturer' => 'Espressif', 'device' => 'ESP8266', 'chip_id' => 'ABC111', 'wifi_mac' => 'aa:bb:cc:dd:ee:fa', 'state_id' => 1],
+            ['name' => 'Test device 2', 'address' => 'Test address 2', 'manufacturer' => 'Espressif', 'device' => 'ESP8266', 'chip_id' => 'ABC222', 'wifi_mac' => 'aa:bb:cc:dd:ee:fb', 'state_id' => 2],
+            ['name' => 'Test device 3', 'address' => 'Test address 3', 'manufacturer' => 'Espressif', 'device' => 'ESP8266', 'chip_id' => 'ABC333', 'wifi_mac' => 'aa:bb:cc:dd:ee:fc', 'state_id' => 3],
+        ];
+
+        foreach ($devices as $key => $value) {
+            Device::create($value);
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(AddDeviceStates::class);
+        $this->call(AddDevice::class);
     }
 }
